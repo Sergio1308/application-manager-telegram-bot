1ee7726f1348cc5ef136a5e5506eba14d5e29ee2
Sergei <sergi.haiduk@gmail.com>
2022-12-22 08:04:17 +0200

    minor code changes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from aiogram.utils import executor
 from config import dp
-from bot.modules.handlers import message_handler as msg, callback_handler as c, application_creation as ac, \
-                                    application_deletion as ad  # todo
+from bot.modules.handlers import message_handler, callback_handler, application_creation, application_deletion
 from bot.db.database import create_table
 
 
@@ -13,9 +12,9 @@ async def on_startup(_):
 
 if __name__ == '__main__':
     # todo
-    msg.init_message_handler(dp)
-    ac.init_app_creation_handlers(dp)
-    ad.init_app_deletion_handlers(dp)
-    c.init_callback_handler(dp)
+    message_handler.init_message_handler(dp)
+    callback_handler.init_callback_handlers(dp)
+    application_creation.init_app_creation_handlers(dp)
+    application_deletion.init_app_deletion_handlers(dp)
     #
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
