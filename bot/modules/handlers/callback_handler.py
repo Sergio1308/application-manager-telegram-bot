04f152cd09129da63b49aab2e6bf281d6b15916c
Sergei <sergi.haiduk@gmail.com>
2022-12-23 05:25:15 +0200

    changed main_menu func

--- bot/modules/handlers/callback_handler.py
+++ bot/modules/handlers/callback_handler.py
@@ -1,5 +1,5 @@
 from aiogram import types, Dispatcher
-from bot.modules.keyboard import inline_start_keyboard, inline_keyboard_creator
+from bot.modules.keyboard import inline_keyboard_creator
 from aiogram.dispatcher import FSMContext
 from bot.modules.states import Forms
 from bot.db.database import add_new_column, get_all_columns, delete_column
@@ -10,7 +10,7 @@ application_models_messages = {}
 
 # region CREATE_APPLICATION
 async def main_menu(call: types.CallbackQuery, state: FSMContext):
-    await call.message.edit_text('Вы вернулись в главное меню\nВыберите пункт 👇', reply_markup=inline_start_keyboard())
+    await call.message.answer('Вы вернулись в главное меню.', reply_markup=types.ReplyKeyboardRemove())
     await state.finish()
     await call.answer()
 
@@ -79,7 +79,7 @@ async def execute_deletion(call: types.CallbackQuery, state: FSMContext):
 
 
 def init_callback_handlers(disp: Dispatcher):
-    disp.register_callback_query_handler(main_menu, text='main_menu', state='*')
+    disp.register_callback_query_handler(main_menu, text='main_menu', state=Forms.application_model)
     disp.register_callback_query_handler(specify_section, text='create_application', state=None)
     disp.register_callback_query_handler(enter_section_name, text=sections, state=Forms.section)
     disp.register_callback_query_handler(insert_data, text='insert_query', state=Forms.application_model)
