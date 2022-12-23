6928092103914246bec31d91022a8489b505041f
Sergei <sergi.haiduk@gmail.com>
2022-12-23 05:40:41 +0200

    added callback data variables

---
+++ bot/modules/handlers/callback_data_vars.py
@@ -0,0 +1,7 @@
+CREATE_APPLICATION = 'create_application'
+DELETE_APPLICATION = 'delete_application'
+MAIN_MENU = 'main_menu'
+INSERT_DATA = 'insert_query'
+DELETE_ENTRY = 'deletion_confirmation'
+DELETE_COLUMN = 'delete_column'
+CANCEL_COLUMN_DELETION = 'cancel_deletion'
