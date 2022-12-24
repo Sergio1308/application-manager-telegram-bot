d4c443675aac8021496e75a6c16298e42ee07da9
Sergei <sergi.haiduk@gmail.com>
2022-12-24 06:14:11 +0200

    added docs

--- bot/modules/keyboard/inline_keyboard_creator.py
+++ bot/modules/keyboard/inline_keyboard_creator.py
@@ -2,6 +2,15 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 def create_inline_keyboard(texts: list, callbacks: list, row_width=1) -> InlineKeyboardMarkup:
+    """
+    A method that creates inline keyboard markup with specified text and callbacks data as a list, so we can
+    use this method to create 1 or more buttons in a markup.
+
+    :param texts: button text list
+    :param callbacks: callback_data text
+    :param row_width: markup row width, by default = 1
+    :return: InlineKeyboardMarkup
+    """
     return InlineKeyboardMarkup(row_width=row_width).add(
         *[InlineKeyboardButton(text=text, callback_data=callback) for text, callback in zip(texts, callbacks)]
     )
